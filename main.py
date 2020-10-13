fd070ecdf1e8e7fe9365dce8dcf521228e5f8024
TomasRojas <rojaslaluz.tomas@gmail.com>
2020-10-13 17:24:04 -0300

    Nearest neighbor implementado
    Ahora queda knn. También tiene un test a fuerza bruta por el array (arr_closest_point)

--- main.py
+++ main.py
@@ -38,14 +38,38 @@ class KDtree:
             return
         orderedData = sorted(points, key=operator.itemgetter((dim + 1) % self.dimensions))
         median = orderedData[len(orderedData) // 2]  # Se consigue la mediana de la lista
-        newNode = KDnode(median, dim+1, len(orderedData) // 2)  # Se crea el nodo raíz usando el valor obtenido
+        newNode = KDnode(median, dim + 1, len(orderedData) // 2)  # Se crea el nodo raíz usando el valor obtenido
         if direction == 0:
             node.left = newNode
         else:
             node.right = newNode
         leftlist, rightlist = split_list(orderedData)
-        self.expandNode(newNode, leftlist, dim+1, 0)
-        self.expandNode(newNode, rightlist, dim+1, 1)
+        self.expandNode(newNode, leftlist, dim + 1, 0)
+        self.expandNode(newNode, rightlist, dim + 1, 1)
+
+    def distance_sqrd(self, point1, point2):
+        distance = []
+        value = 0
+
+        for k in range(0, self.dimensions):
+            distance.append(point1[k] - point2[k])
+
+        for k in range(0, self.dimensions):
+            value += distance[k] * distance[k]
+
+        return value
+
+    def closer_distance(self, pivot, p1, p2):
+        if p1 is None:
+            return p2
+
+        if p2 is None:
+            return p1
+
+        d1 = self.distance_sqrd(pivot, p1)
+        d2 = self.distance_sqrd(pivot, p2)
+
+        return p1 if d1 < d2 else p2
 
     class Printer:
         def __init__(self, text):
@@ -78,13 +102,66 @@ class KDtree:
             self.traverseNodes(printer, padding, pointerLeft, node.left, sibling)
             self.traverseNodes(printer, padding, pointerRight, node.right, False)
 
+    def arr_closest_point(self, all_points, new_point):
+        best_point = None
+        best_distance = None
+
+        for current_point in all_points:
+            current_distance = self.distance_sqrd(new_point, current_point)
+
+            if best_distance is None or current_distance < best_distance:
+                best_distance = current_distance
+                best_point = current_point
+
+        return best_point
+
+    def kd_closest_point(self, point):
+        return self.closest_point(self.rootNode, point)
+
+    def closest_point(self, root, point, depth=0):
+        if root is None:
+            return None
+
+        axis = depth % self.dimensions
+
+        next_branch = None
+        opposite_branch = None
+
+        if point[axis] < root.content[axis]:
+            next_branch = root.left
+            opposite_branch = root.right
+        else:
+            next_branch = root.right
+            opposite_branch = root.left
+
+        best = self.closer_distance(point,
+                                    self.closest_point(next_branch,
+                                                       point,
+                                                       depth + 1),
+                                    root.content)
+
+        if self.distance_sqrd(point, best) > (point[axis] - root.content[axis]) ** 2:
+            best = self.closer_distance(point,
+                                        self.closest_point(opposite_branch,
+                                                           point,
+                                                           depth + 1),
+                                        best)
+
+        return best
+
     def knn(self, point, k):
         pass
 
 
 if __name__ == "__main__":
-    points = [[5,8,7],[3,4,9],[9,2,7],[7,8,1],[0,9,9],[1,1,2],[8,7,9],[2,5,7],[0,1,5],[1,5,2],[6,5,3],[3,3,3],[7,7,4],[5,4,5],[0,0,0]]
-    alt = [[7,2],[2,3],[5,4],[9,6],[4,7],[8,1]]
-    orderedData = sorted(points, key=operator.itemgetter(0))
-    kd = KDtree(points, 3)
+    points = [[5, 8, 7], [3, 4, 9], [9, 2, 7], [7, 8, 1], [0, 9, 9], [1, 1, 2], [8, 7, 9], [2, 5, 7], [0, 1, 5],
+              [1, 5, 2], [6, 5, 3], [3, 3, 3], [7, 7, 4], [5, 4, 5], [0, 0, 0]]
+    alt = [[7, 2], [2, 3], [5, 4], [9, 6], [4, 7], [8, 1]]
+    test = [[4,7],[11,10],[9,4],[15,3],[7,13],[16,10],[14,11]]
+    orderedData = sorted(test, key=operator.itemgetter(0))
+    kd = KDtree(test, 2)
     kd.printTree()
+
+    point = [14,9]
+    print(kd.arr_closest_point(test, point))
+    print(kd.kd_closest_point(point))
