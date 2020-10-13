6afd671193d34fec1ad86dbddd485c15f9ce1af3
Leonardo <leonardo.duran.s@mail.pucv.cl>
2020-10-12 23:12:48 -0300

    usando el Kdtree creado por el tomas

--- dataInput.py
+++ dataInput.py
@@ -1,8 +1,66 @@
 import pandas as pd
 import numpy as np
-from sklearn.neighbors import KDTree
+#from sklearn.neighbors import KDTree
+
+import operator
 
 
+class KDnode:
+    def __init__(self, content, depth, split):
+        self.content = content
+        self.depth = depth
+        self.split = split
+        self.left = None
+        self.right = None
+
+
+def split_list(originalList):
+    half = len(originalList) // 2
+    return originalList[:half], originalList[half + 1:]
+
+
+class KDtree:
+    def __init__(self, data, dimensions):
+        self.rootNode = None
+        self.data = []
+        self.data = data
+        self.dimensions = dimensions
+        self.generateTree()
+
+    def generateTree(self):
+        orderedData = sorted(self.data,
+                             key=operator.itemgetter(0))  # Ordena la lista con respecto a la primera dimension
+        median = orderedData[len(orderedData) // 2]  # Se consigue la mediana de la lista
+        rootNode = KDnode(median, 0, len(orderedData) // 2)  # Se crea el nodo raíz usando el valor obtenido
+        self.rootNode = rootNode
+        leftlist, rightlist = split_list(orderedData)
+        self.expandNode(rootNode, leftlist, 0, 0)
+        self.expandNode(rootNode, rightlist, 0, 1)
+
+    def expandNode(self, node, points, dim, direction):
+        if not points:
+            return
+        orderedData = sorted(points, key=operator.itemgetter((dim + 1) % self.dimensions))
+        median = orderedData[len(orderedData) // 2]  # Se consigue la mediana de la lista
+        newNode = KDnode(median, dim+1, len(orderedData) // 2)  # Se crea el nodo raíz usando el valor obtenido
+        if direction == 0:
+            node.left = newNode
+        else:
+            node.right = newNode
+        leftlist, rightlist = split_list(orderedData)
+        self.expandNode(newNode, leftlist, dim+1, 0)
+        self.expandNode(newNode, rightlist, dim+1, 1)
+
+    def printTree(self):
+        print(self.rootNode.content)
+
+
+'''if __name__ == "__main__":
+    points = [[5,8,7],[3,4,9],[9,2,7],[7,8,1],[0,9,9],[1,1,2],[8,7,9],[2,5,7],[0,1,5],[1,5,2],[6,5,3],[3,3,3],[7,7,4],[5,4,5],[0,0,0]]
+    alt = [[7,2],[2,3],[5,4],[9,6],[4,7],[8,1]]
+    orderedData = sorted(points, key=operator.itemgetter(0))
+    kd = KDtree(points, 3)
+    kd.printTree()'''
 
 
 class DataInput:
@@ -82,18 +140,21 @@ class DataInput:
                 new_df = group
         return new_df
     
-def diez_mas_cercanos(genreMatrix):
+'''def diez_mas_cercanos(genreMatrix):
         
     #print('datos en el arreglo',self.contentRatingMatrix)
     tree = KDTree(genreMatrix,leaf_size=2)
     dist,ind = tree.query(genreMatrix[:1], k=10)
     print(ind)
-    print(dist)
+    print(dist)'''
 
+def diez_mas_cercanos_KDTree(genreMatrix):
+    tree = KDtree(genreMatrix,10)
+    tree.printTree()
 
 def main():
     print('Hola bienvenido')
-    nombre_archivo = '\\Users\\Leonardo\\Documents\\Clases EDA Avanzada\\Desafio EDA\\desafio 3\\OII443_Desafio_3-Jose\\Desafio3.csv'
+    nombre_archivo = 'Desafio3.csv'
     #data = DataInput("\\Users\\Leonardo\\Documents\\Clases EDA Avanzada\\Desafio EDA\\desafio 3\\OII443_Desafio_3-Jose\\Desafio3.csv") 
     data = DataInput(nombre_archivo)
     data.createGenreMatrix()
@@ -102,7 +163,7 @@ def main():
     #data.imprimirDataPD()
     #elemento = data.getElementoPD_id(282935706)
     #print(elemento)
-    diez_mas_cercanos(data.genreMatrix)
+    diez_mas_cercanos_KDTree(data.contentRating)
     #data.imprimirDataNP()
 if __name__ == "__main__":
     main()
